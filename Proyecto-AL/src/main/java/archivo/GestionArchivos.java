e5448dc74d82aef45e51dfb19914124c8de353c0
M4RC0 <marcopoolchichesapon@gmail.com>
2023-08-20 00:41:39 -0600

    gestion de archivos

--- Proyecto-AL/src/main/java/archivo/GestionArchivos.java
+++ Proyecto-AL/src/main/java/archivo/GestionArchivos.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package archivo;
 
-/**
- *
- * @author Usuario
- */
+import java.io.*;
+
 public class GestionArchivos {
-    
+    FileInputStream entrada;
+    FileOutputStream salida;
+    File archivo;
+
+    public GestionArchivos(){
+    }
+// METODO PARA LOS ARCHIVOS
+    public String AbrirATexto(File archivo){
+        String contenido = "";
+        try {
+            entrada = new FileInputStream(archivo);
+            int afk;
+            while ((afk = entrada.read()) != -1){
+                char caracter = (char)afk;
+                contenido += caracter;
+            }
+        }catch (Exception e){
+
+        }
+        return contenido;
+    }
+// METODO PARA GUARDAR EL ARCHIVO (ES OPCIONAL)
+    public String GuardarATexto(File archivo, String contenido){
+        String respuesta = null;
+        try {
+            salida = new FileOutputStream(archivo);
+            byte [] bytesTxt = contenido.getBytes();
+            salida.write(bytesTxt);
+            respuesta = "Se guardo con exito el archivo";
+        }catch (Exception e){
+        }
+        return respuesta;
+    }
 }
